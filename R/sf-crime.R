c8752d957d4c4dc1379620e2b12e606cec6fbf4d
olaf.goerlitz <olaf.goerlitz@gmail.com>
2015-11-13 23:55:43 +0100

    Improved data prep, including date handling and address splitting

--- R/sf-crime.R
+++ R/sf-crime.R
@@ -1,10 +1,38 @@
-# Basic data analysis
+library(ggplot2)
 
-# load data
+# LOAD DATA
 setwd("..")
 train <- read.csv("data/train.csv", header = TRUE)
 
+# PREPARE TIME DATA
+train$Date   <- as.Date(train$Dates)
+train$Dates  <- as.POSIXlt(train$Dates, "PST")
+train$Year   <- as.factor(format(train$Dates, "%Y"))
+train$Month  <- as.factor(format(train$Dates, "%m"))
+train$Hour   <- as.factor(format(train$Dates, "%H"))
+train$Minute <- as.factor(format(train$Dates, "%M"))
+
+# remove incomplete year 2015
+train <- subset(train, Year != "2015")
+train$Year <- droplevels(train$Year)
+
+# remove observations with invalid coordinates
+train <- subset(train, Y < 40)
+
+# process street addresses
+train$Junction <- as.factor(regexpr(".* / .*", train$Address) == 1)
+street <- function(s) { x <- unlist(strsplit(s, " of ")); if(length(x) == 2) x[2] else "JUNCTION"}
+blocks <- function(s) { x <- unlist(strsplit(s, " Block ")); if(length(x) == 2) x[1] else "JUNCTION"}
+train$Street <- factor(unlist(lapply(as.character(train$Address), street)))
+train$Block <- factor(unlist(lapply(as.character(train$Address), blocks)))
+
+
+# top crime and city hot spot
+topCrime <- sort(table(train$Category), decreasing = TRUE)[1]
+hotSpot <- names(sort(table(train$Address), decreasing = TRUE))[1]
+
 # get an overview
+dim(train)
 summary(train)
 summary(train$Category)
 
@@ -13,18 +41,24 @@ x <- as.numeric(train$Category)
 summary(x)
 summary(train$DayOfWeek)
 
+#length(object) # number of elements or components
+#str(object)    # structure of an object
+#class(object)  # class or type of an object
+#names(object)  # names
+
 # analyze crime categories
 crimeCat <- table(train$Category)
 sortedCat <- sort(crimeCat, decreasing = TRUE)
-
 par(las=2) # make label text perpendicular to axis
 par(mar=c(15,4,4,2)) # increase x-axis margin.
 barplot(table(factor(train$Category, levels = names(sortedCat))))
 
-# analyze crime dates
-crimeDate <- as.Date(train$Dates)
-crimeYears <- as.numeric(format(crimeDate, "%Y"))
-crimeMonths <- as.numeric(format(crimeDate, "%m"))
-crimeDays <- as.numeric(format(crimeDate, "%d"))
-trainDates <- data.frame(crimeYears, crimeMonths, crimeDays)
-          
+qplot(train$Category) + theme(axis.ticks = element_blank(), panel.grid.major.y = element_blank()) + coord_cartesian(ylim = c(0,200000)) + coord_flip()
+qplot(train$Category) + theme(axis.ticks = element_blank(), panel.grid.major.y = element_blank()) + scale_x_discrete(limits=rev(names(sortedCat))) + coord_flip()
+qplot(train$Category, fill=train$Category) + theme(axis.ticks = element_blank(), panel.grid.major.y = element_blank()) + scale_x_discrete(limits=rev(names(sortedCat))) + coord_flip()
+qplot(train$Category, fill=..count..) + theme(axis.ticks = element_blank(), panel.grid.major.y = element_blank()) + scale_x_discrete(limits=rev(names(sortedCat))) + coord_flip()
+
+# analyse VEHICLE THEFT
+barplot(table(as.Date(train$Dates[train$Category == "VEHICLE THEFT"])))
+#barplot(table(train[train$Category == "VEHICLE THEFT",]$Year))
+
