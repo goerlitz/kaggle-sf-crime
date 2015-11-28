b2b120fbf7ef11e93e54e0c300648dabcae475d4
olaf.goerlitz <olaf.goerlitz@gmail.com>
2015-11-28 18:02:07 +0100

    Added graphs as part of explorative analysis

--- R/sf-crime.R
+++ R/sf-crime.R
@@ -1,8 +1,8 @@
 library(ggplot2)
+library(lattice)
 
 # LOAD DATA
-setwd("..")
-train <- read.csv("data/train.csv", header = TRUE)
+train <- read.csv("../data/train.csv", header = TRUE)
 
 # PREPARE TIME DATA
 train$Date   <- as.Date(train$Dates)
@@ -46,12 +46,45 @@ summary(train$DayOfWeek)
 #class(object)  # class or type of an object
 #names(object)  # names
 
+# -----------------
+# GRAPHS
+# -----------------
+
+# count crimes per Category and Day
+counts <- as.data.frame(table(train$Year, train$Category))
+colnames(counts) <- c("Year", "Category", "Freq")
+xyplot(Freq ~ Year | Category, data = counts, type = c("p","r"),
+       xlab = "Year", ylab = "Frequency", main = "Number of Crimes per Year",
+       scales=list(x=list(rot=90))
+       )
+
+par(las=2) # make label text perpendicular to axis
+
+# show coordinates with district highlighting
+bayview <- subset(train, train$PdDistrict == "BAYVIEW")
+with(bayview, plot(X, Y, col = rgb(0,0,0,0.1), pch=19))
+qplot(X, Y, data = train, color = PdDistrict, size=1) + scale_size(range = c(0, 2)) + theme_light()
+ggplot(train, aes(X, Y, color = PdDistrict)) + geom_point(size=1.5, alpha=0.1) + theme_light()
+qplot(X, Y, data = train, color = Category)
+with(train, smoothScatter(X, Y))
+
+xyplot(Y ~ X | PdDistrict, data = train, groups = PdDistrict) # plot each district separately colored by district
+xyplot(Y ~ X | Category, data = train, groups = PdDistrict) # plot each category separately colored by district
+histogram(~ Dates | Category, data = train) # each category in histogram
+
 # analyze crime categories
 crimeCat <- table(train$Category)
 sortedCat <- sort(crimeCat, decreasing = TRUE)
 par(las=2) # make label text perpendicular to axis
 par(mar=c(15,4,4,2)) # increase x-axis margin.
 barplot(table(factor(train$Category, levels = names(sortedCat))))
+
+# show per date
+ggplot(data = train, aes(x=Date)) + geom_bar()
+ggplot(data = train, aes(X, Y, color = Category)) + geom_point(size = 1) + theme_light()
+
+vtheft <- subset(train, train$Category == "VEHICLE THEFT")
+qplot(vtheft$PdDistrict, vtheft$DayOfWeek)
 
 qplot(train$Category) + theme(axis.ticks = element_blank(), panel.grid.major.y = element_blank()) + coord_cartesian(ylim = c(0,200000)) + coord_flip()
 qplot(train$Category) + theme(axis.ticks = element_blank(), panel.grid.major.y = element_blank()) + scale_x_discrete(limits=rev(names(sortedCat))) + coord_flip()
@@ -61,4 +94,11 @@ qplot(train$Category, fill=..count..) + theme(axis.ticks = element_blank(), pane
 # analyse VEHICLE THEFT
 barplot(table(as.Date(train$Dates[train$Category == "VEHICLE THEFT"])))
 #barplot(table(train[train$Category == "VEHICLE THEFT",]$Year))
+
+# ----------------
+# Predictions
+# ----------------
+
+# Decision Tree
+
 
